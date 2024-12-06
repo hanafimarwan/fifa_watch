2f7b4bce8dcf6ee154135894013e28137556a6b3
hanafimarwan <habafimarwan@gmail.com>
2024-12-06 17:19:59 +0100

    last commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,12 +19,12 @@ module.exports = {
       },
       keyframes: {
         slideRightOut: {
-          '0%': { transform: 'translateX(0)', opacity: '1' },
-          '100%': { transform: 'translateX(100%)', opacity: '0' },
+          '0%': { transform: 'translateY(0)', opacity: '1' },
+          '100%': { transform: 'translateY(100%)', opacity: '0' },
         },
         slideLeftIn: {
-          '0%': { transform: 'translateX(-100%)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
         },
       }
     },
